cad1ac10316f394e435dca3026180b2c72c908c5
Jan Blechta <blechta@karlin.mff.cuni.cz>
2022-03-03 11:08:15 +0100

    Drop missing file

---
+++ setenv.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Source this file to prepare environment for running on MRZ
+# machines on TU Chemnitz
+
+# NB: It is actually enough to place `gmsh' executable on PATH
+source ~/opt/gmsh-4.5.6/gmsh.conf
+
+export PATH="/usr/shared/packages/matlabr2020b/bin:$PATH"
