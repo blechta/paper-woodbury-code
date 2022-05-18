f758bc0d5a3bebf9657be324710ee37fec20cc6c
Jan Blechta <jan.blechta@math.tu-chemnitz.de>
2022-05-18 12:44:16 +0200

    Tweak appearance of 2d plot

--- +app_dc/+checkerboard_example/postprocess3.m
+++ +app_dc/+checkerboard_example/postprocess3.m
@@ -12,6 +12,7 @@ function compose_resistivity_figure_2d(tag, ns)
     infile = sprintf('%s-n%%04d.fig', tag);
     outfile = sprintf('%s.pdf', tag);
 
+    % Positioning and sizing parameters
     aspect_ratio = numel(ns)/2 * 0.5;
     hspace = 0.02;
     cbar_height = 0.08;
@@ -20,6 +21,13 @@ function compose_resistivity_figure_2d(tag, ns)
     dpi = 92;
     paperw = 5.125;
     marker_sizes = dpi/92 * [12, 8, 3, 1, 0.5, 0, 0, 0, 0];
+    line_width = 0.15;
+    fontsize = 9.5;
+    cbar_pos = [0.1, 1-0.7*cbar_height, 0.75, 0.2*cbar_height];
+    anno_pos = [cbar_pos(1)+cbar_pos(3)+0.0025, cbar_pos(2)-0.000128, ...
+                aspect_ratio*cbar_pos(4), cbar_pos(4)-0.000432];
+    line_x = [anno_pos(1)+0.5*anno_pos(3), anno_pos(1)+0.5*anno_pos(3)];
+    line_y = [anno_pos(2)-0.01, anno_pos(2)-0.001];
 
     fig = figure();
     axs = gobjects(0);
@@ -45,7 +53,8 @@ function compose_resistivity_figure_2d(tag, ns)
         marker_size = marker_sizes(row);
         if marker_size > 0
             hold on;
-            plot(electrode_coords(1, :), electrode_coords(2, :), 'k|', 'MarkerSize', marker_size);
+            plot(electrode_coords(1, :), electrode_coords(2, :), 'k|', ...
+                 'MarkerSize', marker_size, 'LineWidth', line_width);
             hold off;
         end
 
@@ -67,20 +76,17 @@ function compose_resistivity_figure_2d(tag, ns)
         caxis(ax, [lo, hi]);
     end
 
+
     cbar = colorbar('Location', 'northoutside', 'TickLabelInterpreter', 'latex');
     cbar.FontName = 'cmr';
-    cbar.FontSize = 12;
+    cbar.FontSize = fontsize;
     cbar.TickLabelInterpreter = 'latex';
-    %cbar.Box = 'off';
-    %cbar.LineWidth = 1e-323;
-    %cbar.TickLength = 0;
-    cbar.Position = [0.1, 1-0.7*cbar_height, 0.75, 0.2*cbar_height];
-    anno_position = [0.86, 1-0.7*cbar_height, 0.2*cbar_height*aspect_ratio, 0.2*cbar_height];
-    line_x = [0.86 + 0.1*cbar_height*aspect_ratio, 0.86 + 0.1*cbar_height*aspect_ratio];
-    line_y = [1-0.7*cbar_height - 0.01, 1-0.7*cbar_height - 0.001];
-    annotation(fig, 'rectangle', anno_position, 'FaceColor', 'r', 'LineStyle', 'none');
+    cbar.LineWidth = line_width;
+    cbar.Position = cbar_pos;
+    annotation(fig, 'rectangle', anno_pos, 'FaceColor', 'r', 'LineStyle', 'none');
     annotation('textarrow', line_x, line_y, 'String', '7000', 'HeadStyle', 'none', ...
-               'FontName', 'cmr', 'FontSize', 12, 'Interpreter', 'latex', 'TextMargin', 0.05);
+               'FontName', 'cmr', 'FontSize', fontsize, 'Interpreter', 'latex', ...
+               'TextMargin', 0.05, 'LineWidth', line_width);
 
     set(fig, 'PaperUnits', 'points');
     set(fig, 'PaperSize', dpi*[paperw, paperw*aspect_ratio]);
